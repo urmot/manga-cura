c72722c1dc183d0d9adc34c3f6c4dd361520849a
jianghan <jh.shuai@gmail.com>
2016-06-07 09:37:54 +0200

    init

---
+++ db/migrate/20160607072027_create_tests.rb
@@ -0,0 +1,10 @@
+class CreateTests < ActiveRecord::Migration
+  def change
+    create_table :tests do |t|
+      t.string :name
+      t.integer :age
+
+      t.timestamps null: false
+    end
+  end
+end
