452b97c0e07aded7bca1aead13ab27293b9be7ce
yuta-muramoto <s1513114@u.tsukuba.ac.jp>
2016-06-13 23:45:00 +0900

    rename books to comics

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,23 @@
 class UsersController < ApplicationController
+  def show
+
+  end
+
+  def create
+
+  end
+
+  def sign_in
+
+  end
+
+  def sign_up
+
+  end
+
+  def
+
+  def config
+
+  end
 end
