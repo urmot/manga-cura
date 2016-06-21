59d43d0e32b598fa0f9ebdba97d56b82cd656081
DSE-MBP-13-6 <dse-mbp-13-6@DSEMBP136noMBP.d.dblab.is.tsukuba.ac.jp>
2016-06-21 16:01:29 +0900

    ユーザー登録

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,15 +4,17 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
 
+        if @user.save
+          redirect_to @user
+        else
+          render 'new'
+        end
   end
 
   def new
-
-  end
-
-  def create
-
+    @user = User.new
   end
 
   def login
@@ -24,4 +26,11 @@ class UsersController < ApplicationController
   def config
 
   end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:pass, :name, :male, :birthday, :email)
+  end
+
 end
