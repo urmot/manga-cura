bd6bb78af71d746edcbd60d7961a556ac821540a
Nabechi <chacharlie030@gmail.com>
2016-06-28 14:39:10 +0900

    本の詳細機能

--- app/controllers/comics_controller.rb
+++ app/controllers/comics_controller.rb
@@ -4,11 +4,15 @@ class ComicsController < ApplicationController
   end
 
   def show
-
+    @comic = Book.find(params[:id])
   end
 
   def favorite
-    
+    mura = User.first
+    comic = Book.find(1)
+    mura.books << comic
+    mura.save
+
   end
 
   def search
