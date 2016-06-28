06c4da5b2a07c89267a2e206c7b243d57607fd1f
yuripoko <l9ncl.wll@gmail.com>
2016-06-28 14:37:43 +0900

    User_form2

--- db/migrate/20160613215622_create_users.rb
+++ db/migrate/20160613215622_create_users.rb
@@ -2,8 +2,7 @@ class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
       t.string :name, null: false
-      #t.string :password, null: false
-      #t.string :password_confirmation, null: false
+      t.string :password_digest, null: false
       t.string :gender
       t.date :birthday
       t.string :email, null: false
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160623154348) do
+ActiveRecord::Schema.define(version: 20160623153736) do
 
   create_table "authors", force: :cascade do |t|
     t.string   "name",       null: false
@@ -94,15 +94,14 @@ ActiveRecord::Schema.define(version: 20160623154348) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "pass",            null: false
     t.string   "name",            null: false
+    t.string   "password_digest", null: false
     t.string   "gender"
     t.date     "birthday"
     t.string   "email",           null: false
     t.boolean  "notice_confide"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
-    t.string   "password_digest"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
