d132f20d825642370aae4646d3dfbb4b0f045658
jianghan0 <jh.wk.dev@gmail.com>
2016-08-01 00:16:48 +0900

    handing error

--- app/controllers/comics_controller.rb
+++ app/controllers/comics_controller.rb
@@ -16,6 +16,16 @@ class ComicsController < ApplicationController
       get_comics_by_title(params[:query]) : get_comics_by_author(params[:query])
   end
 
+  def read
+    user_id = session[:user_id] #ログインしたユーザのID
+    story_id = params[:read][:story_id] #特定の本のID
+    url = params[:read][:url]
+    read = Read.find_or_create_by(story_id: story_id, user_id: user_id)
+    if read.save
+      redirect_to url
+    end
+  end
+
   private
 
   def get_comics_by_title(query)
@@ -25,16 +35,5 @@ class ComicsController < ApplicationController
   def get_comics_by_author(query)
     author = Author.find_by("name like :name", name: "\%#{query}\%")
     author.blank? ? nil : author.comics
-  end
-
-  def read
-    user_id = session[:user_id] #ログインしたユーザのID
-    story_id = params[:read][:story_id] #特定の本のID
-    url = params[:read][:url]
-    read = Read.find_or_create_by(story_id: story_id, user_id: user_id)
-    if read.save
-      redirect_to url
-    end
-
   end
 end
